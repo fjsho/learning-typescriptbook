ea30b57f94d5e5e514c320c02a1e7d6d4d4fe520
Your Name <ID+USERNAME@users.noreply.github.com>
2025-04-27 19:37:58 +0900

    Reactでいいねボタンを作ろう
    https://typescriptbook.jp/tutorials/react-like-button-tutorial

---
+++ react-like-button-tutorial/like-button/src/App.tsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <LikeButton />
+    </>
+  );
+
+}
+
+function LikeButton() {
+  const [count, setCount] = useState(999);
+
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+
+  return <span className="likeButton" onClick={handleClick}>❤︎ {count}</span>
+}
+
+export default App
